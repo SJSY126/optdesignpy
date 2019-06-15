f0c7f2c50058ac8fcac7d8bcc2d35b965e0b80b3
SJSY <soya.saijo@gmail.com>
2019-06-15 16:54:43 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-
-# Learn more: https://github.com/kennethreitz/setup.py
-
 from setuptools import setup, find_packages
-
 
 with open('README.rst') as f:
     readme = f.read()
@@ -12,14 +7,14 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
-    version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    name='optdesignpy',
+    version='0.0.1',
+    description='Tools for optical design',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='SJSY126',
+    author_email='soya.saijo@gmail.com',
+    install_requests=['numpy', 'matplotlib'],
+    url='https://github.com/SJSY126/optdesignpy',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
-
